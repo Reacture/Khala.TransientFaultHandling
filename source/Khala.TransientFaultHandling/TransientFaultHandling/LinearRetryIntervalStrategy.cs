9bd105cc2f9660a96747be565e81e16d60597ce8
Gyuwon <gyuwon@live.com>
2017-08-27 22:52:24 +0900

    LinearRetryIntervalStrategy 클래스를 추가합니다.

---
+++ source/Khala.TransientFaultHandling/TransientFaultHandling/LinearRetryIntervalStrategy.cs
@@ -0,0 +1,31 @@
+﻿namespace Khala.TransientFaultHandling
+{
+    using System;
+
+    public class LinearRetryIntervalStrategy : RetryIntervalStrategy
+    {
+        public LinearRetryIntervalStrategy(
+            TimeSpan initialInterval,
+            TimeSpan increment,
+            TimeSpan maximumInterval,
+            bool immediateFirstRetry)
+            : base(immediateFirstRetry)
+        {
+            InitialInterval = initialInterval;
+            Increment = increment;
+            MaximumInterval = maximumInterval;
+        }
+
+        public TimeSpan InitialInterval { get; }
+
+        public TimeSpan Increment { get; }
+
+        public TimeSpan MaximumInterval { get; }
+
+        protected override TimeSpan GetIntervalFromTick(int tick) =>
+            TimeSpan.FromTicks(
+                Math.Min(
+                    MaximumInterval.Ticks,
+                    InitialInterval.Ticks + (Increment.Ticks * (tick - 1))));
+    }
+}
