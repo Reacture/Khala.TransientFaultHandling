c6263d5d07e3f081b00e9b76ab0cfdac487b6a6d
Gyuwon <gyuwon@live.com>
2017-08-27 16:30:13 +0900

    프로젝트를 추가합니다.

---
+++ source/Khala.TransientFaultHandling.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("818a1b9f-3237-479c-ae35-219e97bbd8c0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source/Khala.TransientFaultHandling/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: Guid("b2aad428-5b56-465b-95b0-f038dd6c0ebb")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
