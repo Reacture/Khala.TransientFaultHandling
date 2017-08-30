1f9ecff6677508d99256f849b6011f210522a77c
Gyuwon <gyuwon@live.com>
2017-08-30 23:56:21 +0900

    DelegatingRetryIntervalStrategy 클래스를 추가합니다.

---
+++ source/Khala.TransientFaultHandling/TransientFaultHandling/DelegatingRetryIntervalStrategy.cs
@@ -0,0 +1,20 @@
+﻿namespace Khala.TransientFaultHandling
+{
+    using System;
+
+    public class DelegatingRetryIntervalStrategy : RetryIntervalStrategy
+    {
+        private readonly Func<int, TimeSpan> _func;
+
+        public DelegatingRetryIntervalStrategy(Func<int, TimeSpan> func, bool immediateFirstRetry)
+            : base(immediateFirstRetry)
+        {
+            _func = func ?? throw new ArgumentNullException(nameof(func));
+        }
+
+        protected override TimeSpan GetIntervalFromTick(int tick)
+        {
+            return _func.Invoke(tick);
+        }
+    }
+}
