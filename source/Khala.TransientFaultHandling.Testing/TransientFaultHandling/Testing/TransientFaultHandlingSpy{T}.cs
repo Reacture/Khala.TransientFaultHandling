4cf5b66e40e41964378350d14c87c3d1e213035e
Gyuwon <gyuwon@live.com>
2017-09-08 11:30:22 +0900

    TransientFaultHandlingSpy<T> 클래스에 취소 토큰 연산과 콜백을 적용합니다.

--- source/Khala.TransientFaultHandling.Testing/TransientFaultHandling/Testing/TransientFaultHandlingSpy{T}.cs
+++ source/Khala.TransientFaultHandling.Testing/TransientFaultHandling/Testing/TransientFaultHandlingSpy{T}.cs
@@ -1,18 +1,26 @@
 ﻿namespace Khala.TransientFaultHandling.Testing
 {
     using System;
+    using System.Threading;
     using System.Threading.Tasks;
 
     public class TransientFaultHandlingSpy<T>
     {
         private static readonly Random _random = new Random();
 
+        private readonly Func<CancellationToken, Task<T>> _callback;
         private readonly int _maximumRetryCount;
         private readonly int _transientFaultCount;
         private int _invocationCount;
 
         public TransientFaultHandlingSpy()
+            : this(cancellationToken => Task.FromResult(default(T)))
         {
+        }
+
+        public TransientFaultHandlingSpy(Func<CancellationToken, Task<T>> callback)
+        {
+            _callback = callback;
             _maximumRetryCount = _random.Next(1000, 2000);
             _transientFaultCount = _random.Next(0, _maximumRetryCount);
             _invocationCount = 0;
@@ -23,11 +31,14 @@
                 new ConstantRetryIntervalStrategy(TimeSpan.Zero, immediateFirstRetry: true));
 
             OperationNonCancellable = Operation;
+            OperationCancellable = Operation;
         }
 
         public RetryPolicy<T> Policy { get; }
 
-        public Func<Task<T>> OperationNonCancellable { get; } = () => Task.FromResult(default(T));
+        public Func<Task<T>> OperationNonCancellable { get; }
+
+        public Func<CancellationToken, Task<T>> OperationCancellable { get; }
 
         public void Verify()
         {
@@ -39,13 +50,23 @@
             throw new InvalidOperationException("It seems that operation did not invoked by retry policy.");
         }
 
-        private Task<T> Operation()
+        private Task<T> Operation() => Operation(CancellationToken.None);
+
+        private async Task<T> Operation(CancellationToken cancellationToken)
         {
             _invocationCount++;
 
+            try
+            {
+                await _callback.Invoke(cancellationToken);
+            }
+            catch
+            {
+            }
+
             if (_invocationCount == _transientFaultCount + 1)
             {
-                return Task.FromResult(default(T));
+                return default(T);
             }
 
             throw new InvalidOperationException("Transient fault occured. Try more please.");
--- source/Khala.TransientFaultHandling.Tests/TransientFaultHandling/Testing/TransientFaultHandlingSpyT_specs.cs
+++ source/Khala.TransientFaultHandling.Tests/TransientFaultHandling/Testing/TransientFaultHandlingSpyT_specs.cs
@@ -2,13 +2,20 @@
 {
     using System;
     using System.Reflection;
+    using System.Threading;
     using System.Threading.Tasks;
     using FluentAssertions;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
 
     [TestClass]
     public class TransientFaultHandlingSpyT_specs
     {
+        public interface IFunctionProvider
+        {
+            TResult Func<T, TResult>(T arg);
+        }
+
         [TestMethod]
         public void sut_is_immutable()
         {
@@ -31,6 +38,14 @@
         {
             var sut = new TransientFaultHandlingSpy<Result>();
             Func<Task<Result>> actual = sut.OperationNonCancellable;
+            actual.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public void OperationCancellable_returns_Func_CancellationToken_Task_instance()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>();
+            Func<CancellationToken, Task<Result>> actual = sut.OperationCancellable;
             actual.Should().NotBeNull();
         }
 
@@ -68,6 +83,103 @@
             Action action = sut.Verify;
 
             action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public async Task given_OperationCancellable_invoked_by_Policy_Verify_succeeds()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>();
+            await sut.Policy.Run(sut.OperationCancellable, CancellationToken.None);
+
+            Action action = sut.Verify;
+
+            action.ShouldNotThrow();
+        }
+
+        [TestMethod]
+        public void given_OperationCancellable_not_invoked_Verify_throws_InvalidOperationException()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>();
+            Action action = sut.Verify;
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public async Task given_OperationCancellable_invoked_directly_Verify_throws_InvalidOperationException()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>();
+            try
+            {
+                await sut.OperationCancellable.Invoke(CancellationToken.None);
+            }
+            catch
+            {
+            }
+
+            Action action = sut.Verify;
+
+            action.ShouldThrow<InvalidOperationException>();
+        }
+
+        [TestMethod]
+        public async Task OperationNonCancellable_invokes_callback_with_none_cancellation_token()
+        {
+            var functionProvider = Mock.Of<IFunctionProvider>();
+            var sut = new TransientFaultHandlingSpy<Result>(
+                functionProvider.Func<CancellationToken, Task<Result>>);
+
+            try
+            {
+                await sut.OperationNonCancellable.Invoke();
+            }
+            catch
+            {
+            }
+
+            Mock.Get(functionProvider).Verify(x => x.Func<CancellationToken, Task<Result>>(CancellationToken.None), Times.Once());
+        }
+
+        [TestMethod]
+        public void OperationNonCancellable_consumes_callback_exception()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>(
+                cancellationToken => throw new NotImplementedException());
+
+            Func<Task> action = () => sut.OperationNonCancellable.Invoke();
+
+            action.ShouldNotThrow<NotImplementedException>();
+        }
+
+        [TestMethod]
+        [DataRow(true)]
+        [DataRow(false)]
+        public async Task OperationCancellable_invokes_callback(bool canceled)
+        {
+            var cancellationToken = new CancellationToken(canceled);
+            var functionProvider = Mock.Of<IFunctionProvider>();
+            var sut = new TransientFaultHandlingSpy<Result>(
+                functionProvider.Func<CancellationToken, Task<Result>>);
+
+            try
+            {
+                await sut.OperationCancellable.Invoke(cancellationToken);
+            }
+            catch
+            {
+            }
+
+            Mock.Get(functionProvider).Verify(x => x.Func<CancellationToken, Task<Result>>(cancellationToken), Times.Once());
+        }
+
+        [TestMethod]
+        public void OperationCancellable_consumes_callback_exception()
+        {
+            var sut = new TransientFaultHandlingSpy<Result>(
+                cancellationToken => throw new NotImplementedException());
+
+            Func<Task> action = () => sut.OperationCancellable.Invoke(CancellationToken.None);
+
+            action.ShouldNotThrow<NotImplementedException>();
         }
 
         public class Result
