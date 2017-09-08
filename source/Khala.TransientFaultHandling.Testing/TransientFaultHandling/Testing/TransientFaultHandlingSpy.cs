cf29f484677c8358b3605b6529fceada2897c048
Gyuwon <gyuwon@live.com>
2017-09-08 10:21:21 +0900

    TransientFaultHandlingSpy 클래스를 추가합니다.

---
+++ source/Khala.TransientFaultHandling.Testing/TransientFaultHandling/Testing/TransientFaultHandlingSpy.cs
@@ -0,0 +1,54 @@
+﻿namespace Khala.TransientFaultHandling.Testing
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class TransientFaultHandlingSpy
+    {
+        private static readonly Random _random = new Random();
+
+        private readonly int _maximumRetryCount;
+        private readonly int _transientFaultCount;
+        private int _invocationCount;
+
+        public TransientFaultHandlingSpy()
+        {
+            _maximumRetryCount = _random.Next(1000, 2000);
+            _transientFaultCount = _random.Next(0, _maximumRetryCount);
+            _invocationCount = 0;
+
+            Policy = new RetryPolicy(
+                _maximumRetryCount,
+                new TransientFaultDetectionStrategy(),
+                new ConstantRetryIntervalStrategy(TimeSpan.Zero, immediateFirstRetry: true));
+
+            OperationNonCancellable = Operation;
+        }
+
+        public RetryPolicy Policy { get; }
+
+        public Func<Task> OperationNonCancellable { get; }
+
+        public void Verify()
+        {
+            if (_invocationCount == _transientFaultCount + 1)
+            {
+                return;
+            }
+
+            throw new InvalidOperationException("It seems that operation did not invoked by retry policy.");
+        }
+
+        private Task Operation()
+        {
+            _invocationCount++;
+
+            if (_invocationCount == _transientFaultCount + 1)
+            {
+                return Task.FromResult(true);
+            }
+
+            throw new InvalidOperationException("Transient fault occured. Try more please.");
+        }
+    }
+}
