5eb8e58c34904f6e9074d2ffedd5961b276bfbb2
Gyuwon Yi <gyuwon@live.com>
2017-10-06 22:17:11 +0900

    TransientFaultHandlingExtensions 확장 메서드 클래스를 추가합니다.

--- source/Khala.TransientFaultHandling/TransientFaultHandling/TransientFaultDetectionStrategy{T}.cs
+++ source/Khala.TransientFaultHandling/TransientFaultHandling/TransientFaultDetectionStrategy{T}.cs
@@ -2,6 +2,8 @@
 {
     public class TransientFaultDetectionStrategy<T> : TransientFaultDetectionStrategy
     {
+#pragma warning disable IDE0034 // Ignore "Simplify 'default' expression" warning because Equals() method is non generic.
         public virtual bool IsTransientResult(T result) => default(bool);
+#pragma warning restore IDE0034 // Ignore "Simplify 'default' expression" warning because Equals() method is non generic.
     }
 }
