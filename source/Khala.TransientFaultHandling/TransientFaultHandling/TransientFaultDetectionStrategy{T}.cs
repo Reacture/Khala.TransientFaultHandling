cc2a9503211461bce50cb5d369b22fd0c4c2da78
Gyuwon <gyuwon@live.com>
2017-10-11 10:25:36 +0900

    TransientFaultDetectionStrategy<T> 클래스에서 불필요한 코드를 삭제합니다.

--- source/Khala.TransientFaultHandling/TransientFaultHandling/TransientFaultDetectionStrategy{T}.cs
+++ source/Khala.TransientFaultHandling/TransientFaultHandling/TransientFaultDetectionStrategy{T}.cs
@@ -2,8 +2,6 @@
 {
     public class TransientFaultDetectionStrategy<T> : TransientFaultDetectionStrategy
     {
-#pragma warning disable IDE0034 // Ignore "Simplify 'default' expression" warning because Equals() method is non generic.
-        public virtual bool IsTransientResult(T result) => default(bool);
-#pragma warning restore IDE0034 // Ignore "Simplify 'default' expression" warning because Equals() method is non generic.
+        public virtual bool IsTransientResult(T result) => false;
     }
 }
